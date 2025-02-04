c45d182ebe54f6d537e37f822388ccc723f0b05c
Hillary Nyakundi <hillarynyakundi66@gmail.com>
2025-02-04 20:06:36 +0300

    fix(ui):Added semantic tags and new structuring to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,23 @@ import type { Metadata } from "next";
 import { poppins } from "@/app/ui/fonts";
 import "@/global.css";
 
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
 export const metadata: Metadata = {
   title: "Pets-Kernel",
   description: "Buy pets, vend pets feed and find lost pets",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={poppins.className}>
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
